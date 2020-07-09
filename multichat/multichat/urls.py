95d0c059eb31ef72da33be1b8f313cb9683b69dc
Stephen Wolff <stephen@maxgatedigital.com>
2020-07-09 14:20:41 +0100

    Fix multichat non-docker runserver for Django < 3

--- multichat/multichat/urls.py
+++ multichat/multichat/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from chat.views import index
 
 
 urlpatterns = [
     path('', index),
-    path('accounts/login/', login),
-    path('accounts/logout/', logout),
+    path('accounts/login/', LoginView.as_view()),
+    path('accounts/logout/', LogoutView.as_view()),
     path('admin/', admin.site.urls),
 ]
